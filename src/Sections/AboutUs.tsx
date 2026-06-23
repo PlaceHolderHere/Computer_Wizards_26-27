6bed34a165e4ecbbe361685544f856b066d08420
PlaceHolderHere <holderhereinplace@gmail.com>
2026-06-23 22:56:40 +0800

    Added some proper content to about-us and adjusted the styling

--- src/Sections/AboutUs.tsx
+++ src/Sections/AboutUs.tsx
@@ -3,30 +3,18 @@ import './AboutUs.css'
 function AboutUs(){
     return(
         <section className="about" id="about">
-            <h2>About Us</h2>
+            <h2 className="title">About Us</h2>
             <div className="aboutGrid">
                 <div className='imageSection'>
                     <img 
                         className="about-photo" 
-                        src="src/assets/workshop.jpg" 
-                        alt=""
+                        src="src/assets/about-us-1.jpg" 
+                        alt="About Us 1"
                     />
                 </div>
                 <div className='description'>
-                    <h3>Title here</h3>
-                    <p>Placeholder description: Led by experienced professionals, these workshops provide the opportunity for students to learn from professional experience and gain knowledge about the industry. Hands-on and real time projects allow students to further develop their technical skills and gain experience exposure to the tech industry under the guidance of professionals.</p>
-                </div>
-
-                <div className='description'>
-                    <h3>Title here</h3>
-                    <p>Placeholder description: Led by experienced professionals, these workshops provide the opportunity for students to learn from professional experience and gain knowledge about the industry. Hands-on and real time projects allow students to further develop their technical skills and gain experience exposure to the tech industry under the guidance of professionals.</p>
-                </div>
-                <div className='imageSection'>
-                    <img 
-                        className="about-photo" 
-                        src="src/assets/workshop.jpg" 
-                        alt=""
-                    />
+                    <h3>The Official Computer Club of AdDU-SHS</h3>
+                    <p>Here at the Computer Wizards Club, we strive to help curious students develop their programming skills and fundamentals through hands-on club meetings, activities, and workshops.</p>
                 </div>
             </div>
         </section>
