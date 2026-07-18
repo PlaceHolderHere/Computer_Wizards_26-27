a92f773a87657617f593bbb78788d4e337873618
PlaceHolderHere <holderhereinplace@gmail.com>
2026-07-18 23:44:40 +0800

    Added a basic hover color change effect to the navbar

--- src/svgLogos/menu.tsx
+++ src/svgLogos/menu.tsx
@@ -6,7 +6,7 @@ interface menuProps{
 function MenuSVG({width, height}: menuProps){
     return(
         <svg width={`${width}px`} height={`${height}px`} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M4 6H20M4 12H20M4 18H20" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
     )
 
